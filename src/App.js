cabc3c1f79f0d49ab0685ccb6ffe35d433043d36
kwangwei <kwangwei@sol59.com>
2023-05-25 20:19:12 +0800

    modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Grid from "./Grid";
 
+
+const originalFetch = window.fetch;
+
+// Override the global fetch function with your custom implementation
+window.fetch = function(url, options) {
+  console.log(url);
+  // Modify the 'url' variable here with your desired changes
+  // For example, prepend a proxy URL or modify headers
+  const urlWithoutProtocol = url.replace(/^(https?:\/\/)/, '');
+  // Example modification: prepend a proxy URL
+  const modifiedUrl = 'https://queryticket.000webhostapp.com/electra.php?uri=' + urlWithoutProtocol;
+
+  // Make the actual fetch request with the modified URL and options
+  return originalFetch(modifiedUrl, options);
+};
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('https://electra-https.radisys.workers.dev/')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
